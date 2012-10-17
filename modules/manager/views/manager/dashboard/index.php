aae5859117369728bb870019c8e07823a6eb4441
Henrique Boaventura <hboaventura@gmail.com>
2012-10-17 18:26:22 -0300

    removido o modulo manager e colocado ele como um submodulo. closes #7

--- modules/manager/views/manager/dashboard/index.php
+++
@@ -1,7 +0,0 @@
-<br />
-<br />
-<br />  
-<div class="hero-unit">
-  <h1>Dashboard</h1>
-  <p>Olá <strong><?php echo Auth::instance()->get_user()->name; ?></strong>! Seja bem-vindo ao sistema de gerenciamento do site.</p>  
-</div>
