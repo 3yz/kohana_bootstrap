cc862e236549c3acbfffc6cd269390d711d6e824
Joel Souza <joel.souza@gmail.com>
2012-12-17 17:38:39 -0200

    h5bp assets, h5bp .htaccess, underscore-min.js

---
+++ public/js/main.combined.js
@@ -0,0 +1,8 @@
+<!--#include file="vendor/jquery-1.8.3.min.js" -->
+<!--#include file="vendor/underscore-min.js" -->
+<!--#include file="vendor/console.log.js" -->
+
+// jquery DOM-ready function
+// $(function(){
+
+// });
